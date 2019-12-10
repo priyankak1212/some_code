833b0629635d3959d29456828ae021678ce24cc0
priyankak1212 <56244830+priyankak1212@users.noreply.github.com>
2019-12-10 14:28:38 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 
-echo "this was run automagically"
+echo "this was run automatically"
+echo "Congratulations for Success!"
